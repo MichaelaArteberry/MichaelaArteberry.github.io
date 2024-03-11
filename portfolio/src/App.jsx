92a4009e2d7ae2717abf825ed44249fc6218a54d
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-03-11 14:37:55 -0400

    Added Navbar and hamburger menu, added media queries for the nav

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.module.css'
+import styles from './App.module.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
-    <>
-      <div className={styles.App}>Hello</div>
-    </>
-  )
+    <div className={styles.App}>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App
