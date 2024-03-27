020921b213933a7f4b7380996f2698a762f629dc
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-03-27 19:40:14 -0400

    Added CSS, just need to fix over all color scheme

--- portfolio/src/components/Experience/Experience.jsx
+++ portfolio/src/components/Experience/Experience.jsx
@@ -1,29 +1,31 @@
 import React from "react";
+import styles from "./Experience.module.css";
 import skills from "../../data/skills";
 import history from "../../data/history";
 import { getImageUrl } from "../../utils";
 
 const Experience = () => {
     return (
-        <section id="experience">
-            <h2>Experiences</h2>
-            <div>
-                <div>
+        <section className={styles.container} id="experience">
+            <h2 className={styles.title}>Experiences</h2>
+            <div className={styles.content}>
+                <div className={styles.skills}>
                     {skills.map((skill, id) => {
                         return (
-                            <div key={id}>
-                                <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
+                            <div key={id} className={styles.skill}>
+                                <div className={styles.skillImageContainer}>
+                                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
                                 <p>{skill.title}</p>
                             </div>
                         );
                     })}
                 </div>
-                <ul>
+                <ul className={styles.history}>
                     {history.map((historyItem, id) => {
                         return (
-                            <li key={id}>
+                            <li key={id} className={styles.historyItem}>
                                 <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organization} Logo`} />
-                                <div>
+                                <div className={styles.historyItemDetails}>
                                     <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
                                     <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                                     <ul>
