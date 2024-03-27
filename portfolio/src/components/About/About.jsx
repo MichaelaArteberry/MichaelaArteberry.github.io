cd11fc1f49db015def4d35ee9da3a1346b8ed0f4
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-03-27 14:28:04 -0400

    Completed the structure of the about section. Still need to adjust colors and information within

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
-import "./About.module.css";
+import styles from "./About.module.css";
 
 const About = () => {
     return (
-        <section>
-            <h2>About Me</h2>
-            <div>
-                <img src={getImageUrl("about/aboutImage.png")} alt="img sitting with laptop"/>
-                <ul>
-                    <li><img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon"/>
-                    <div>
-                        <h3>Frontend Development</h3>
-                        <p>List frontend experiences here. I have experience in building responsive and optimized sites.</p>
-                    </div>
+        <section className={styles.container} id="about">
+            <h2 className={styles.title}>About Me</h2>
+            <div className={styles.content}>
+                <img className={styles.aboutImage} src={getImageUrl("about/aboutImage.png")} alt="img sitting with laptop" />
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Frontend Development</h3>
+                            <p>List frontend experiences here. I have experience in building responsive and optimized sites.</p>
+                        </div>
                     </li>
 
-                    <li><img src={getImageUrl("about/serverIcon.png")} alt="cursor icon"/>
-                    <div>
-                        <h3>Backend Development</h3>
-                        <p>List backend experiences here. I have experience developing fast and optimized back-end systems and API's.</p>
-                    </div>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="cursor icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend Development</h3>
+                            <p>List backend experiences here. I have experience developing fast and optimized back-end systems and API's.</p>
+                        </div>
                     </li>
 
-                    <li><img src={getImageUrl("about/designIcon.png")} alt="cursor icon"/>
-                    <div>
-                        <h3>Other Development</h3>
-                        <p>List other development experiences here. I have experience using Unreal Engine nodes.</p>
-                    </div>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/designIcon.png")} alt="cursor icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Other Development</h3>
+                            <p>List other development experiences here. I have experience using Unreal Engine nodes.</p>
+                        </div>
                     </li>
                 </ul>
-                </div>
+            </div>
         </section>
     );
 }
