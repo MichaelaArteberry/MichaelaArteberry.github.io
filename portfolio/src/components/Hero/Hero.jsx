01d782d8525d03ed9e6092e21555e07e5c6cbb23
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-03-27 21:58:40 -0400

    Deployed portfolio on netlify

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hello, I'm Michaela!</h1>
-                <p className={styles.description}>I'm a full-stack web developer. Fill in the rest later...</p>
+                <p className={styles.description}>As a full-stack software engineer with a deep interest in game development, I combine my technical expertise with the discipline and adaptability gained during my service as an army veteran. Leveraging this unique blend of skills, I am committed to creating engaging experiences that captivate and inspire. Let's turn ideas into reality.</p>
                 <a href="mailto:michaela.arteberry@gmail.com" className={styles.contactBtn}>Contact Me</a>
             </div>
             <img src={getImageUrl('hero/heroImage.PNG')} alt="Michaela Arteberry" className={styles.heroImg} />
