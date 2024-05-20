c7e88151909b46df8f4a72975b322355f94067f7
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-05-20 15:07:26 -0400

    testing resume

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -5,27 +5,45 @@ import { getImageUrl } from '../../utils';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [showResume, setShowResume] = useState(false);
 
-    return <nav className={styles.navbar}>
-        <a target="_blank" className={styles.title} href="public/Resume/Resume.pdf">Resume</a>
-        <div className={styles.menu}>
-
-            <img className={styles.menuBtn} src={
-                menuOpen
-                    ? getImageUrl("nav/closeIcon.png")
-                    : getImageUrl("nav/menuIcon.png")
-            }
-                alt="menu-button"
-                onClick={() => setMenuOpen(!menuOpen)} />
-
-            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-    </div>
-    </nav >
+    return (
+<div>
+            <nav className={styles.navbar}>
+                <button
+                    className={styles.title}
+                    onClick={() => setShowResume(!showResume)}
+                >
+                    Resume
+                </button>
+                <div className={styles.menu}>
+                    <img
+                        className={styles.menuBtn}
+                        src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
+                        alt="menu-button"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    />
+                    <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#experience">Experience</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+            {showResume && (
+                <div className={styles.resumeContainer}>
+                    <iframe
+                        src="/Resume/Resume.pdf"
+                        width="100%"
+                        height="600px"
+                        title="Resume"
+                    >
+                    </iframe>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Navbar;
---
+++ portfolio/src/components/ResumeViewer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ResumeViewer = () => {
+    return (
+        <div>
+            <iframe
+                src="/Resume/Resume.pdf"
+                width="100%"
+                height="1000px"
+            >
+            </iframe>
+        </div>
+    );
+}
+
+export default ResumeViewer;
