8257cadbf0c7da0cd1b9bf07bb9ec56bd2677c0c
Michaela Arteberry <michaela.arteberry@gmail.com>
2025-03-27 11:20:04 -0400

    made some cosmetic changes to the link in about me

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -31,8 +31,8 @@ const About = () => {
                         <img src={getImageUrl("about/designIcon.png")} alt="cursor icon" />
                         <div className={styles.aboutItemText}>
                             <h3>Other Development</h3>
-                            <p>I have experience using Unreal Engine's Blueprint visual scripting system, to create a third-person target shooter game. I explored core game play mechanics such as player movement, aiming and shooting. Experimented with animation blueprints and trigger based actions. My Github repository has the project files {" "}.
-                            <a href="https://github.com/MichaelaArteberry/First-Unreal-Game" target="_blank" rel="noopener noreferrer">here</a>
+                            <p className={styles.link}>I have experience using Unreal Engine's Blueprint visual scripting system, to create a third-person target shooter game. I explored core game play mechanics such as player movement, aiming and shooting. Experimented with animation blueprints and trigger based actions. My Github repository has the project files {" "}
+                            <a href="https://github.com/MichaelaArteberry/First-Unreal-Game" target="_blank" rel="noopener noreferrer">here.</a>
                             </p>
                             <p>I am one semester away from completing my undergraduate degree in Computer Science. I have been studying C++, and focusing on object oriented programming, algorithms, and data strictures. I have completed a full stack software engineering program from Hack Reactor in May 2024 that focused on Python and JavaScript. </p>
                         </div>
